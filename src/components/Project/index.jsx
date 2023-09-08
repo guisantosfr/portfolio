e223fe8717debf4c7d24066ece23f6394abac58b
Francisco Guilherme <santosgui678@gmail.com>
2023-09-08 11:00:25 -0300

    More responsive adjustments

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,34 +1,29 @@
 import './styles.css'
 
-function Project({ project, index } ){
+function Project({ project, index }) {
   const isOdd = index % 2 !== 0
 
-  return(
+  return (
     <article className={isOdd ? 'reverse' : null}>
-        <div className="text">
-          <h3>{project.name}</h3>
+      <div className="text">
+        <h3>{project.name}</h3>
 
-          <p className="black-box">
-            {project.description}
-          </p>
+        <p className="black-box">
+          {project.description}
+        </p>
 
-          <ul>
-            <li><a href={project.sourceCode} className="btn" target="_blank">Código Fonte</a></li>
-            {
-              project.liveUrl !== null ?
-                <li><a href={project.liveUrl} className="btn" target="_blank">Página</a></li> :
-                null
-            }
-            {
-              project.linkedinPost !== null ?
-                <li><a href={project.linkedinPost} className="btn" target="_blank">Post no Linkedin</a></li> :
-                null
-            }
-          </ul>
-        </div>
+        <ul>
+          <li><a href={project.sourceCode} className="btn" target="_blank">Código Fonte</a></li>
+          {
+            project.liveUrl !== null ?
+              <li><a href={project.liveUrl} className="btn" target="_blank">Página</a></li> :
+              null
+          }
+        </ul>
+      </div>
 
-        <img src={project.img} alt={`Screenshot of ${project.name}`} loading="lazy" />
-      </article>
+      <img src={project.img} alt={`Screenshot of ${project.name}`} loading="lazy" />
+    </article>
   )
 }
 
