f72ca4b933e56feafa748b33b3c22c90b83c7656
Guilherme Santos <santosgui678@gmail.com>
2024-09-01 11:35:31 -0300

    Finalizada seção de projetos

--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,7 @@
 import vueEcommerceMockup from './assets/vue-ecommerce.png';
 import whereInTheWorldMockup from './assets/where-in-the-world.png';
+import loopstudiosMockup from './assets/loopstudios.png';
+import minhaCarteiraMockup from './assets/minha-carteira.gif';
 
 const projects = [
   {
@@ -20,19 +22,19 @@ const projects = [
   },
   {
     id: 3,
-    name: 'Minha carteira',
-    description: 'Dashboard desenvolvido com ReactJs, Typescript e Styled Components',
-    sourceCode: 'https://github.com/guisantosfr/minha-carteira',
-    liveUrl: 'https://minha-carteira-zeta.vercel.app/',
-    img: 'https://user-images.githubusercontent.com/32960040/172261238-4c05ee9c-477b-40e5-a7f2-2396b52e2591.gif',
-  },
-  {
-    id: 4,
     name: 'Loopstudios Landing Page',
     description: 'Desafio nível júnior do Frontend Mentor. Desenvovido com with HTML, SASS e abordagem Mobile-First.',
     sourceCode: 'https://github.com/guisantosfr/Loopstudios-landing-page',
     liveUrl: 'https://guisantosfr.github.io/Loopstudios-landing-page/',
-    img: 'https://user-images.githubusercontent.com/32960040/237445971-38212a79-e97f-4c67-b861-abd97856d733.JPG',
+    img: loopstudiosMockup
+  },
+  {
+    id: 4,
+    name: 'Minha carteira',
+    description: 'Dashboard desenvolvido com ReactJs, Typescript e Styled Components',
+    sourceCode: 'https://github.com/guisantosfr/minha-carteira',
+    liveUrl: 'https://minha-carteira-zeta.vercel.app/',
+    img: minhaCarteiraMockup
   }
 ]
 
