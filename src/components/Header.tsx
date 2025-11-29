6a32aadb946069f1d083ac17dcb1f5f6f7ffe94e
Guilherme Santos <santosgui678@gmail.com>
2025-11-29 10:43:09 -0300

    Adicionando links no header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,15 @@ import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLaptopCode, faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons';
 import { faWhatsapp, faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
- 
+
 //import './styles.css'
 
 export default function Header() {
   return (
     <nav className="font-mono text-[90%] py-12 px-4 md:px-8 lg:px-12 lg:max-w-screen-xl lg:mx-auto">
-      <ul className="list-none p-0 m-0 flex flex-wrap justify-center items-center gap-9">
-        <li className="w-full text-center lg:w-auto lg:text-left lg:mr-auto">
+
+      <ul className="flex flex-wrap justify-between items-center gap-9">
+        <li className="w-full text-center lg:w-auto lg:text-left">
           <h1>
             <Link href="/" className="font-sans text-white no-underline block hover:text-[#1061e3] text-[150%]">
               <FontAwesomeIcon icon={faLaptopCode} aria-hidden="true" className='mr-2' />
@@ -18,21 +19,23 @@ export default function Header() {
           </h1>
         </li>
 
-        <li>
+        <li className='flex gap-10'>
+          <a href="#projects">Projetos</a>
+          <a href="#skills">Habilidades</a>
+          <a href="#contact">Contato</a>
+        </li>
+
+        <li className='flex gap-10'>   
           <Link href="https://api.whatsapp.com/send?phone=5586988775657" target="_blank" className="text-white no-underline block hover:text-[#1061e3]">
             <FontAwesomeIcon icon={faWhatsapp} aria-hidden="true" className='mr-2 text-[175%] text-[#86fbfb] hover:text-[#1061e3]' title="Whatsapp" />
             <span className="sr-only">Whatsapp</span>
           </Link>
-        </li>
 
-        <li>
           <Link href="https://www.linkedin.com/in/guisantosfr" target="_blank" className="text-white no-underline block hover:text-[#1061e3]">
             <FontAwesomeIcon icon={faLinkedin} aria-hidden="true" className='mr-2 text-[175%] text-[#86fbfb] hover:text-[#1061e3]' title="Linkedin"/>
             <span className="sr-only">LinkedIn</span>
           </Link>
-        </li>
 
-        <li>
           <Link href="https://www.github.com/guisantosfr" target="_blank" className="text-white no-underline block hover:text-[#1061e3]">
             <FontAwesomeIcon icon={faGithubSquare} aria-hidden="true" className='mr-2 text-[175%] text-[#86fbfb] hover:text-[#1061e3]' title="Github" />
             <span className="sr-only">Github</span>
--- src/components/Techs.tsx
+++ src/components/Techs.tsx
@@ -2,7 +2,7 @@ import { Tech } from '@/types/tech';
 
 export default function Techs({ mainTechs, secondaryTechs }: { mainTechs: Tech[]; secondaryTechs: Tech[] }) {
     return (
-      <section id="techs" className="bg-[#0d224b]">
+      <section id="skills" className="bg-[#0d224b]">
         <div className="max-w-md sm:max-w-lg mx-auto text-center py-12">
           <h2 className="text-2xl sm:text-4xl">Tecnologias Principais</h2>
   
